0c88c0c339706583e294deb5e9fd16d6f3a69106
Norman Wilson <wilsonnorman@yahoo.com>
2017-01-31 18:59:51 -0500

    text

--- src/roomCal.java
+++ src/roomCal.java
@@ -3,36 +3,69 @@ import java.util.Scanner;
 
 /**
  * Created by wilsonnorman on 1/30/2017.
+ * This code in to calculates the area of a room
  */
 public class roomCal {
     public static void main (String [] args) {
+
+        // Set up coding
         Scanner scan = new Scanner (System.in);
+        // Tell user what to expect
+        System.out.println("Come one, come all and calculate your room with a push of a button");
+
         System.out.println("Perimeter");
 
+        String answer = "y";
 
-        //3. ask for input
-        System.out.print("Please enter length of classroom: ");
-        double length = scan.nextDouble();
+        while (answer.equals("y")) {
 
-        System.out.print("Please enter width of classroom: ");
-        double width = scan.nextDouble();
+            // ask the user or input
+            System.out.print("Please enter length of classroom: ");
+            double length = scan.nextDouble();
 
-        double perimeter = 2 * length + 2 * width;
+            System.out.print("Please enter width of classroom: ");
+            double width = scan.nextDouble();
 
-        //4. perform calculations
-        double area = length * width;
+            double perimeter = 2 * length + 2 * width;
 
-        //5. output results
-        System.out.println("The area is " + area);
+            //calculations
+            double area = length * width;
 
-        //6. close out resources
-        scan.close();
+            //results
+            System.out.println("The area is " + area);
 
-        System.out.println("length: " + length);
-        System.out.println("Width: " + width);
-        System.out.println("Area: " + area);
-        System.out.println ("perimeter" + perimeter );
-        System.out.println("Continue?" +  " (y/n)" );
+
+            System.out.println("length: " + length);
+            System.out.println("Width: " + width);
+            System.out.println("Area: " + area);
+            System.out.println("perimeter " + perimeter);
+
+            //6. close coding out
+            System.out.println("Continue?" + " (y/n)");
+            answer = scan.next();
+        }
+
+
+              scan.close();
+
+             System.out.println("Thank you, and have a wonderful day");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
